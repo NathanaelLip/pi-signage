871b34fa7941e32ac4f06c48369c7351e0d6fa31
NathanaelLip <nathanael.lipchurch@gmail.com>
2025-04-09 08:57:34 -0400

    1.0

--- install.sh
+++ install.sh
@@ -39,6 +39,42 @@
 #   --overscroll-history-navigation=0 \
 #   --disable-pinch
 
+# Nginx
+sudo apt update
+sudo apt upgrade
+
+sudo apt remove apache2
+
+sudo apt install nginx
+
+sudo systemctl start nginx
+
+# PHP
+sudo apt install php7.4-fpm php7.4-mbstring php7.4-mysql php7.4-curl php7.4-gd php7.4-curl php7.4-zip php7.4-xml -y
+
+sudo nano /etc/nginx/sites-enabled/default
+# add index.php to line where it says "Add index.php to the list if you are using PHP"
+
+# Find
+#location ~ \.php$ {
+        #       include snippets/fastcgi-php.conf;
+        #
+        #       # With php5-cgi alone:
+        #       fastcgi_pass 127.0.0.1:9000;
+        #       # With php5-fpm:
+        #       fastcgi_pass unix:/var/run/php5-fpm.sock;
+        #}
+
+# Replace with
+# location ~ \.php$ {
+#                include snippets/fastcgi-php.conf;
+#                fastcgi_pass unix:/var/run/php/php8.2-fpm.sock;
+#         }
+
+sudo systemctl reload nginx
+
+
+# Setup chrome
 sudo apt-get update
 sudo apt-get upgrade
 
